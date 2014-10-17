066747d07523627cd296de8e011d8df0b3caa75b
andkrist <k.strupen@gmail.com>
2014-10-17 11:59:11 +0200

    Project kickoff
    Starting files for our project.
    desktop contains launchable main.

---
+++ desktop/src/com/profiletailor/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.profiletailor.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.profiletailor.game.PT2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PT2(), config);
+	}
+}
